f0f57dd3b9bf8213c14c7c8e3b96cc8edfb31f5f
max <maxduburck@hotmail.com>
2025-04-03 16:54:59 +0200

    added cbc encryption mode, though it doesnt work for challenge_10.

--- src/aes_128.rs
+++ src/aes_128.rs
@@ -53,12 +53,88 @@ pub fn decrypt_aes128_ecb_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyho
         .flat_map(|(i, block)| {
             // If this is the last chunk, remove padding before XOR
             if i == data.len() / key.len() - 1 {
-                utils::bytes_xor(remove_padding(block), key)
+                remove_padding(&utils::bytes_xor(block, key)).to_owned()
             } else {
                 utils::bytes_xor(block, key)
             }
         })
         .collect())
+}
+
+///CBC encryption uses the previously encrypted block to encrypt the next block. Since the first
+///block will not have a predecessor, an initialization vector is needed to make this encryption
+///method work.
+/// WARN: The IV and padding scheme used in this function are not secure, this function should not
+/// be used in practice.
+pub fn encrypt_aes128_cbc_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+    let mut result = Vec::new();
+    let mut previous_encrypted_block = vec![0u8; key.len()]; // this is the IV
+
+    //for every block, xor with previous block, then xor it with the key to encrypt,
+    //if the last block is shorter than the block size, padding will be added.
+    for block in data.chunks(key.len()) {
+        let padded_block = if block.len() == key.len() {
+            block.to_vec()
+        } else {
+            utils::add_padding(block, key.len())
+        };
+        let block_to_encrypt = utils::bytes_xor(&padded_block, &previous_encrypted_block);
+        let ciphertext_block = utils::bytes_xor(&block_to_encrypt, key);
+        result.extend_from_slice(&ciphertext_block);
+        previous_encrypted_block = ciphertext_block;
+    }
+
+    Ok(result)
+}
+
+///for decrypting, we use te same steps as the encryption function, only reversed.
+/// WARN: The IV and padding scheme used in this function are not secure, this function should not
+/// be used in practice.
+pub fn decrypt_aes128_cbc_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+    fn remove_padding(slice: &[u8]) -> &[u8] {
+        // Count the number of repeating bytes at the end of the slice
+        let n_repeating_end = slice
+            .iter()
+            .rev()
+            .take_while(|&n| *n == slice[slice.len() - 1])
+            .count();
+        // Return the slice without the repeating bytes *as a reference*
+        &slice[0..slice.len() - n_repeating_end]
+    }
+    if data.len() % key.len() != 0 {
+        return Err(anyhow!("data length not multiple of key length"));
+    }
+    let mut result = Vec::new();
+    let mut previous_ciphertext_block = vec![0u8; key.len()]; // This is the IV
+
+    for block in data.chunks(key.len()) {
+        // Decrypt the current block
+        let decrypted_block = utils::bytes_xor(block, key);
+
+        // XOR the decrypted block with the previous ciphertext block
+        let plaintext_block = utils::bytes_xor(&decrypted_block, &previous_ciphertext_block);
+        result.extend_from_slice(&plaintext_block);
+
+        // The current ciphertext block becomes the previous for the next iteration
+        previous_ciphertext_block = block.to_vec();
+    }
+
+    // Remove padding from the last block
+    let unpadded_result = remove_padding(&result);
+    Ok(unpadded_result.to_vec())
+}
+
+#[test]
+fn cbc_mode_encrypt_test() -> Result<(), anyhow::Error> {
+    const LOREM: &str = include_str!("../data/lorem_ipsum.txt");
+    const KEY: &str = "YELLOW SUBMARINE";
+
+    let encrypted = encrypt_aes128_cbc_mode(LOREM.as_bytes(), KEY.as_bytes()).unwrap();
+    let result = decrypt_aes128_cbc_mode(&encrypted, KEY.as_bytes()).unwrap();
+    if result != LOREM.as_bytes() {
+        return Err(anyhow!("unexpected result"));
+    }
+    Ok(())
 }
 
 #[test]
@@ -67,8 +143,7 @@ fn test_aes128_2() -> Result<(), anyhow::Error> {
     const KEY: &str = "YELLOW SUBMARINE";
     let encrypted = encrypt_aes128_ecb_mode(LOREM.as_bytes(), KEY.as_bytes());
     let result = decrypt_aes128_ecb_mode(&encrypted, KEY.as_bytes())?;
-    let result = String::from_utf8_lossy(&result);
-    if result != LOREM {
+    if result != LOREM.as_bytes() {
         return Err(anyhow!("unexpected result"));
     }
     Ok(())
--- src/challenge_10.rs
+++ src/challenge_10.rs
@@ -1,44 +1,18 @@
 use crate::aes_128;
-use crate::repeated_xor;
-use crate::single_byte_xor;
 use crate::utils;
+use anyhow::Error;
+use anyhow::anyhow;
 
-//todo
-//CBC mode is a block cipher mode that allows us to encrypt irregularly-sized messages, despite
-//the fact that a block cipher natively only transforms individual blocks.
-//
-//in CBC mode each ciphertext block is added to the next plaintext block before the next call to
-//cipher core.
-//
-//the first plaintext block which has no associated previous ciphertext block, is added to a "fake
-//0th ciphertext block" called the initialization vector, or IV.
-//
-//implement CBC mode by hand by taking the ECB function you wrote earlier, making it encrypt
-//instead of decrypt and using your XOR function from the previous exercise to combine them.
-//
-//verify the encrypt function by decrypting whatever you encrypt to test.
-//
-//the file, saved in data is intelligeble when decrypted against yellow submarine with an IV of
-//all ASCII 0 (\x00\x00\x00 &c)
+fn challenge_10() -> Result<(), anyhow::Error> {
+    const DATA: &str = include_str!("../data/challenge_10.txt");
+    const KEY: &str = "YELLOW SUBMARINE";
+    let result = aes_128::decrypt_aes128_cbc_mode(DATA.as_bytes(), KEY.as_bytes())?;
+    println!("{:?}", String::from_utf8_lossy(&result));
 
-// fn cbc_mode_encrypt(data: &[u8], key: &[u8]) Result<(), error::anyhow {
-//     //creating initialization vector of all ASCII 0
-//     let mut iv: Vec<u8> = Vec::with_capacity(16);
-//     iv.extend_from_slice(&[0u8; 16]);
-//     println!("{:?}", iv);
-//     //make blocks of 16 bytes
-//     let blocks = data.chunks_exact(16);
-//     //xor iv with first block, then encrypt it with key
-//     let initial_block = utils::bytes_xor(blocks.next()?, iv);
-//     let encrypted_block = aes_128::encrypt(t, key, iv, data)
-//     //now xor each
-//
-//     OK(())
-// }
-//
-// #[test]
-// fn cbc_mode_encrypt_test() {
-//     const DATA: &str = include_str!("../data/lorem_ipsum.txt");
-//     const KEY: &[u8] = b"YELLOW SUBMARINE";
-//     cbc_mode_encrypt(DATA.as_bytes(), KEY);
-// }
+    Ok(())
+}
+
+#[test]
+fn challenge_10_test() -> Result<(), anyhow::Error> {
+    challenge_10()
+}
