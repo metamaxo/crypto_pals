0b9b6038b669c52c8adc86b648ddce7df85e38d0
Rubenduburck <rubenduburck@gmail.com>
2025-04-05 18:39:24 +0000

    example of using generic constant to abstract away magic numbers

--- src/challenge_10.rs
+++ src/challenge_10.rs
@@ -8,14 +8,14 @@ use openssl::base64;
 
 fn challenge_10() -> Result<(), anyhow::Error> {
     const DATA: &str = include_str!("../data/challenge_10.txt");
-    const KEY: &[u8] = b"YELLOW SUBMARINE";
+    const KEY: &[u8; 16] = b"YELLOW SUBMARINE";
     let data = base64::decode_block(
         DATA.replace("\n", "")
             .replace("\r", "")
             .replace("\r\n", "")
             .as_ref(),
     )?;
-    let result = aes_128::decrypt_aes128_cbc_mode(&data, KEY)?;
+    let result = crate::aes::decrypt_cbc::<16>(&data, KEY, &mut [0u8; 16])?;
     let string_result = String::from_utf8_lossy(&result);
 
     println!("{:?}", string_result);
