90ce712e8f664b55c248e2eb78592d82bf100e6b
max <maxduburck@hotmail.com>
2025-04-05 15:59:28 +0200

    small edits

--- src/aes_128.rs
+++ src/aes_128.rs
@@ -61,37 +61,37 @@ pub fn decrypt_aes128_ecb_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyho
         .collect())
 }
 
-///CBC encryption uses the previously encrypted block to encrypt the next block. Since the first
-///block will not have a predecessor, an initialization vector is needed to make this encryption
-///method work.
+/// CBC encryption uses the previously encrypted block to encrypt the next block. Since the first
+/// block will not have a predecessor, an initialization vector is needed to make this encryption
+/// method work.
 /// WARN: The IV and padding scheme used in this function are not secure, this function should not
 /// be used in practice.
 pub fn encrypt_aes128_cbc_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
-    let mut previous_encrypted_block = vec![0u8; key.len()]; // this is the IV
+    // Create the IV
+    let mut previous_encrypted_block = vec![0u8; key.len()];
 
-    //for every block, xor with previous block, then xor it with the key to encrypt,
-    //if the last block is shorter than the block size, padding will be added.
-    let result = data
+    // For every block, xor with previous block, then xor it with the key to encrypt,
+    // If the last block is shorter than the block size, padding will be added.
+    Ok(data
         .chunks(key.len())
-        //add padding to each block to ensure they're the same length.
+        // Add padding to each block to ensure they're the same length.
         .map(|block| utils::add_padding(block, key.len()))
-        //first xor the block with previously encrypted ciphertext block. next xor the block with the key
+        // First xor the block with previously encrypted ciphertext block. next xor the block with the key
         .flat_map(|block| {
             let block_to_encrypt = utils::bytes_xor(&block, &previous_encrypted_block);
             let ciphertext_block = utils::bytes_xor(&block_to_encrypt, key);
-            //clone encrypted block so we can use it in the next cycle.
-            previous_encrypted_block = ciphertext_block.clone();
+            // The current ciphertext block becomes the previous for the next iteration.
+            previous_encrypted_block = ciphertext_block.to_vec();
             ciphertext_block
         })
-        .collect();
-
-    Ok(result)
+        .collect())
 }
 
-///for decrypting, we use te same steps as the encryption function, only reversed.
+/// For decrypting, we use te same steps as the encryption function, only reversed.
 /// WARN: The IV and padding scheme used in this function are not secure, this function should not
 /// be used in practice.
 pub fn decrypt_aes128_cbc_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyhow::Error> {
+    // Function to remove padding if needed.
     fn remove_padding(slice: &[u8]) -> &[u8] {
         // Count the number of repeating bytes at the end of the slice
         let n_repeating_end = slice
@@ -102,30 +102,33 @@ pub fn decrypt_aes128_cbc_mode(data: &[u8], key: &[u8]) -> Result<Vec<u8>, anyho
         // Return the slice without the repeating bytes *as a reference*
         &slice[0..slice.len() - n_repeating_end]
     }
+    // Encrypted data must be a multiple of key length
     if data.len() % key.len() != 0 {
         return Err(anyhow!("data length not multiple of key length"));
     }
-    let mut result = Vec::new();
-    let mut previous_ciphertext_block = vec![0u8; key.len()]; // This is the IV
-    for block in data.chunks(key.len()) {
-        // Decrypt the current block
-        let decrypted_block = utils::bytes_xor(block, key);
-
-        // XOR the decrypted block with the previous ciphertext block
-        let plaintext_block = utils::bytes_xor(&decrypted_block, &previous_ciphertext_block);
-        result.extend_from_slice(&plaintext_block);
-
-        // The current ciphertext block becomes the previous for the next iteration
-        previous_ciphertext_block = block.to_vec();
-    }
+    // Create the IV
+    let mut previous_ciphertext_block = vec![0u8; key.len()];
+    // For every block, xor with key to decrypt, then xor it with the previous block to get the
+    // plaintext block.
+    let result: Vec<u8> = data
+        .chunks(key.len())
+        .flat_map(|block| {
+            // Decrypt the current block
+            let decrypted_block = utils::bytes_xor(block, key);
+            // XOR the decrypted block with the previous ciphertext block
+            let plaintext_block = utils::bytes_xor(&decrypted_block, &previous_ciphertext_block);
+            // The current ciphertext block becomes the previous for the next iteration
+            previous_ciphertext_block = block.to_vec();
+            plaintext_block
+        })
+        .collect();
 
     // Remove padding from the last block
-    let unpadded_result = remove_padding(&result);
-    Ok(unpadded_result.to_vec())
+    Ok(remove_padding(&result).to_vec())
 }
 
 #[test]
-fn test_aes128_cbcteode() -> Result<(), anyhow::Error> {
+fn test_aes128_cbc_mode() -> Result<(), anyhow::Error> {
     const LOREM: &str = include_str!("../data/lorem_ipsum.txt");
     const KEY: &str = "YELLOW SUBMARINE";
 
--- src/challenge_10.rs
+++ src/challenge_10.rs
@@ -4,11 +4,12 @@ use crate::traits::BytesHexExt;
 use crate::utils;
 use anyhow::Error;
 use anyhow::anyhow;
+use openssl::base64;
 
 fn challenge_10() -> Result<(), anyhow::Error> {
     const DATA: &str = include_str!("../data/challenge_10.txt");
     const KEY: &[u8] = b"YELLOW SUBMARINE";
-    let data = <Vec<u8>>::try_from_base64(
+    let data = base64::decode_block(
         DATA.replace("\n", "")
             .replace("\r", "")
             .replace("\r\n", "")
