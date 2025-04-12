0b594041f54094555dd97a4dd1cc891e42926ce9
max <maxduburck@hotmail.com>
2025-04-12 16:25:47 +0200

    added padding verification for challenge 15

--- src/aes_128.rs
+++ src/aes_128.rs
@@ -24,6 +24,16 @@ fn unpad_pkcs7(mut data: Vec<u8>) -> Vec<u8> {
     }
     data
 }
+/// Verify PKCS#7 padding
+fn verify_pkcs7(data: &[u8]) -> bool {
+    if let Some(&pad) = data.last() {
+        let pad_usize = pad as usize;
+        if pad_usize > 0 && pad_usize <= data.len() && data.len() % 16 == 0 {
+            return data.iter().rev().take(pad_usize).all(|&byte| byte == pad);
+        }
+    }
+    false
+}
 
 /// Encrypt with AES-128 in CBC mode
 pub fn encrypt_aes128_cbc(data: &[u8], key: &[u8], iv: &mut [u8]) -> Result<Vec<u8>> {
@@ -95,7 +105,6 @@ pub fn decrypt_aes128_ecb(data: &[u8], key: &[u8]) -> Result<Vec<u8>> {
     Ok(unpad_pkcs7(result))
 }
 
-#[test]
 fn test_aes128_cbc_mode() -> Result<(), anyhow::Error> {
     const LOREM: &str = include_str!("../data/lorem_ipsum.txt");
     const KEY: &[u8; 16] = b"YELLOW SUBMARINE";
@@ -114,5 +123,38 @@ fn test_aes128_ecb_mode() -> Result<(), anyhow::Error> {
     let encrypted = encrypt_aes128_ecb(LOREM.as_bytes(), KEY)?;
     let result = decrypt_aes128_ecb(&encrypted, KEY)?;
     utils::require_eq(result.as_slice(), LOREM.as_bytes())?;
+    Ok(())
+}
+
+#[test]
+fn test_pkcs7_verification() -> Result<()> {
+    const UNPADDED: &[u8] = b"ICE ICE BABY";
+    let padded = pad_pkcs7(UNPADDED);
+
+    // Valid padding
+    assert!(verify_pkcs7(&padded), "Valid padding should be verified");
+
+    // Invalid padding: correct length for block size, incorrect padding bytes
+    let invalid_padding_bytes: Vec<u8> = UNPADDED
+        .iter()
+        .copied()
+        .chain(std::iter::repeat(5_u8).take(4))
+        .collect();
+    assert!(
+        !verify_pkcs7(&invalid_padding_bytes),
+        "Invalid padding bytes should not be verified"
+    );
+
+    // Invalid padding: correct padding byte, incorrect padding length
+    let invalid_padding_length: Vec<u8> = UNPADDED
+        .iter()
+        .copied()
+        .chain(std::iter::repeat(2_u8).take(2))
+        .collect();
+    assert!(
+        !verify_pkcs7(&invalid_padding_length),
+        "Invalid padding length should not be verified"
+    );
+
     Ok(())
 }
